ef4e18431e138984a23832b048e94a8d04852132
Maximilian Schmitt <maximilian.schmitt@googlemail.com>
2015-05-04 12:35:48 +0200

    use tabs instead of spaces

--- test/api-mock.js
+++ test/api-mock.js
@@ -3,27 +3,27 @@
 var ApiMock = function() {};
 
 ApiMock.__predefined__ = {
-	login: 'gitclick-test'
+  login: 'gitclick-test'
 };
 
 ApiMock.prototype = {
-	authenticate: function() {},
-	repos: {
-		create: function(request, cb) {
-			cb(null, {
-				name: request.name,
-				owner: {
-					login: ApiMock.__predefined__.login
-				},
-				has_issues: request.has_issues,
-				private: request.private,
-				has_wiki: request.has_wiki,
-				clone_url: 'https://github.com/' + ApiMock.__predefined__.login + '/' + request.name + '/',
-				ssh_url: 'git@github.com:' + ApiMock.__predefined__.login + '/' + request.name + '/',
-				git_url: 'git://github.com/' + ApiMock.__predefined__.login + '/' + request.name + '/'
-			});
-		}
-	}
+  authenticate: function() {},
+  repos: {
+    create: function(request, cb) {
+      cb(null, {
+        name: request.name,
+        owner: {
+          login: ApiMock.__predefined__.login
+        },
+        has_issues: request.has_issues,
+        private: request.private,
+        has_wiki: request.has_wiki,
+        clone_url: 'https://github.com/' + ApiMock.__predefined__.login + '/' + request.name + '/',
+        ssh_url: 'git@github.com:' + ApiMock.__predefined__.login + '/' + request.name + '/',
+        git_url: 'git://github.com/' + ApiMock.__predefined__.login + '/' + request.name + '/'
+      });
+    }
+  }
 };
 
 module.exports = ApiMock;
